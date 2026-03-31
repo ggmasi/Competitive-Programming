b7547f948f351522bbcddc369e5ee533f02ecfcf
ggmasi <ggmasi123@gmail.com>
2026-03-31 15:40:38 -0300

    lucro

---
+++ lucro.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    while(cin >> n){
+        int c; cin >> c;
+        int temp; cin >> temp;
+        int ant = temp-c;
+        int maior = temp-c;
+        for (int i = 1; i < n; i++){
+            cin >> temp;
+            ant = max(temp-c, (ant+temp-c));
+            if(ant > maior){
+                maior = ant;
+            }
+        }
+        if(maior <= 0) maior = 0;
+        cout << maior << endl;
+    }
+}
