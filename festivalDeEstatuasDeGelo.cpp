a6e938577175194fd37f965b2f67c90c8bbc5d5d
ggmasi <ggmasi123@gmail.com>
2026-04-01 10:55:56 -0300

    festival de estatuas de gelo

---
+++ festivalDeEstatuasDeGelo.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n, m; cin >> n >> m;
+        vector<int> dp(m+1, 1e9);
+        dp[0] = 0;
+        vector<int> valores(n);
+        
+        for (int i = 0; i < n; i++){
+            cin >> valores[i];
+        }
+        
+
+        for (int i = 0; i < n; i++){
+            for (int j = valores[i]; j <= m; j++){
+                dp[j] = min(dp[j], 1+dp[j-valores[i]]);
+            }
+        }
+        
+        
+        cout << dp[m] << endl;
+    }
+}
