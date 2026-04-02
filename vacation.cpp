98ca46f23fb27b6548ce94a27310c5c58ff8ec80
ggmasi <ggmasi123@gmail.com>
2026-04-01 21:59:31 -0300

    vacation atualizado

--- vacation.cpp
+++ vacation.cpp
@@ -1,35 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 1e5+5;
 
-int n;
-
-int dp[MAX][3] = { 0 };
-int vet[MAX][3];
-
-int calc(int a, int p){
-    if(p >= n) return 0;
-    if(dp[p][a] != 0) return dp[p][a];
-    dp[p][a] = vet[p][a] + max(calc(((a+1)%3), p+1), calc(((a+2)%3), p+1));
-    return dp[p][a];
-}
 
 int main(){
-    
+    int n;
     cin >> n;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < 3; j++){
-            cin >> vet[i][j];
-        }
-        
-    }
-    dp[n-1][0] = vet[n-1][0];
-    dp[n-1][1] = vet[n-1][1];
-    dp[n-1][2] = vet[n-1][2];
-    int ans = max(calc(0, 0), calc(1,0));
-    ans = max(ans, calc(2, 0));
-
     
+    vector<vector<int>> dp(n, vector<int>(3));
+    cin >> dp[0][0] >> dp[0][1] >> dp[0][2];
+
+    for (int i = 1; i < n; i++){
+        int a, b, c; cin >> a >> b >> c;
+
+        dp[i][0] = a + max(dp[i-1][1], dp[i-1][2]);
+        dp[i][1] = b + max(dp[i-1][0], dp[i-1][2]);
+        dp[i][2] = c + max(dp[i-1][1], dp[i-1][0]);
+    }
+    
+    int ans = max(dp[n-1][0], max(dp[n-1][1], dp[n-1][2]));
     cout << ans << endl;
+    return 0;
 }
