24d1a4e7a3c244725899763f30ed06276db6943d
ggmasi <ggmasi123@gmail.com>
2026-04-02 13:00:34 -0300

    adição de exercícios e gitignore ubuntu

---
+++ comparacaoDeSubstring.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s1, s2;
+    while(getline(cin, s1)){
+        getline(cin, s2);
+
+        vector<vector<int>> dp(s1.length()+1, vector<int>(s2.length()+1, 0));
+        int maior = 0;
+        for (int i = 1; i <= s1.length(); i++){
+            for (int j = 1; j <= s2.length(); j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    if(dp[i][j] > maior) maior = dp[i][j];
+                }
+            }
+            
+        }
+        cout << maior << endl;
+    }
+}
