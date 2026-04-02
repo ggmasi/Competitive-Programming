7e9bc39a7bba564dcf083fc541fd1dcfab7221d8
ggmasi <ggmasi123@gmail.com>
2026-04-01 21:24:31 -0300

    frog1 atualizado

--- frog1.cpp
+++ frog1.cpp
@@ -1,34 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 1e5+5;
-const int INF = INT32_MAX;
-
-
-int n;
-vector<int> heights;
-int dp[MAX];
 
 
 
 int main(){
-    cin >> n;
+    int n; cin >> n;
+
+    vector<int> v(n, INT32_MAX);
+    v[0] = 0;
+    vector<int> pesos(n);
     for (int i = 0; i < n; i++){
-        dp[i] = INF;
+        cin >> pesos[i];
+    }
+
+    for (int i = 0; i < n-1; i++){
+        v[i+1] = min(v[i+1], v[i]+abs(pesos[i]-pesos[i+1]));
+        if(i == n-2) continue;
+        v[i+2] = min(v[i+2], v[i]+abs(pesos[i]-pesos[i+2]));
     }
     
-    for (int i = 0; i < n; i++){
-        int x;
-        cin >> x;
-        heights.push_back(x);
-    }
-    dp[0] = 0;
-
-    for (int i = 1; i < n; i++){
-        dp[i] = min(dp[i], dp[i-1] + abs(heights[i]-heights[i-1]));
-        if(i > 1) dp[i] = min(dp[i], dp[i-2] + abs(heights[i]-heights[i-2]));
-    }
-
-    cout << dp[n-1] << endl;
+    
+    cout << endl << v[n-1] << endl;
     
 }
