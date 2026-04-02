8db8693c9fd413e9ab3bfeb0e394be18f75e81bb
ggmasi <ggmasi123@gmail.com>
2026-04-02 16:05:14 -0300

    lista algoritmos gulosos

---
+++ cedulas.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    int temp = 0;
+    cout << n << endl;
+    
+    temp = n/100; n = n%100;
+    cout << temp << " nota(s) de R$ 100,00" << endl;
+
+    temp = n/50; n = n%50;
+    cout << temp << " nota(s) de R$ 50,00" << endl;
+
+    temp = n/20; n = n%20;
+    cout << temp << " nota(s) de R$ 20,00" << endl;
+
+    temp = n/10; n = n%10;
+    cout << temp << " nota(s) de R$ 10,00" << endl;
+
+    temp = n/5; n = n%5;
+    cout << temp << " nota(s) de R$ 5,00" << endl;
+
+    temp = n/2; n = n%2;
+    cout << temp << " nota(s) de R$ 2,00" << endl;
+
+    temp = n/1; n = n%1;
+    cout << temp << " nota(s) de R$ 1,00" << endl;
+}
