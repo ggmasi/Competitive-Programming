7202338d44d6410e0cd42dd224ba14b49a976457
ggmasi <ggmasi123@gmail.com>
2026-04-08 18:06:47 -0300

    problema do troco

---
+++ dp/minimizingCoins.cpp
@@ -0,0 +1,29 @@
+//https://cses.fi/problemset/task/1634
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int INF = 1e6+7;
+
+int main(){
+    int n, x; cin >> n >> x;
+    vector<int> valores(n);
+    vector<int> dp(x+1, INF);
+    dp[0] = 0;
+
+    for (int i = 0; i < n; i++){
+        cin >> valores[i];
+    }
+    
+
+    for (int i = 0; i < n; i++){
+        for (int j = valores[i]; j <= x; j++){
+            dp[j] = min(dp[j], 1+dp[j-valores[i]]);
+        }
+        
+    }
+    
+    if(dp[x] != INF) cout << dp[x] << endl;
+    else cout << -1 << endl;
+    return 0;
+}
