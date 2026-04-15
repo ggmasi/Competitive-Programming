f23037bd55c0f9c95db028803fe903bba2f53eee
ggmasi <ggmasi123@gmail.com>
2026-04-15 09:05:41 -0300

    coins

--- dp/coins.cpp
+++ dp/coins.cpp
@@ -1,10 +1,12 @@
+//https://atcoder.jp/contests/dp/tasks/dp_i
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     int n; cin >> n;
-    vector<long double> valores(n);
-    for (int i = 0; i < n; i++){
+    vector<long double> valores(n+1);
+    valores[0] = 0;
+    for (int i = 1; i <= n; i++){
         cin >> valores[i];
     }
     
@@ -16,20 +18,21 @@ int main(){
     for (int i = 1; i <= n; i++){
         for (int j = 0; j <= n; j++){
             if(j == 0){
-                dp[i][j] = ((1-valores[i-1])*dp[i-1][j]);
+                dp[i][j] = (1-valores[i])*dp[i-1][j];
             }else{
-                dp[i][j] = (dp[i-1][j] * (1-valores[i-1])) + (dp[i-1][j-1]*valores[i-1]);
+                dp[i][j] = ((dp[i-1][j] * (1-valores[i])) + (dp[i-1][j-1] * (valores[i])));
+
             }
         }
-    }
-    
-    long double ans = 0;
-
-    for (int i = n; i >= maioria; i--){
-        ans += dp[n][i];
+        
     }
     
 
-    cout << fixed << setprecision(10) << ans << "\n";
+    long double sum =0;
+    for (int i = maioria; i <= n; i++){
+        sum += dp[n][i];
+    }
+    
 
+    cout << setprecision(10) << sum << endl;
 }
