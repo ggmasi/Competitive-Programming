291a2367cccb3edb975f03cb782537dc31a896d6
ggmasi <ggmasi123@gmail.com>
2026-04-27 21:01:40 -0300

    Manacher para maior palindromo

---
+++ Outros/subcadeias.cpp
@@ -0,0 +1,51 @@
+//https://neps.academy/br/exercise/2131
+#include <bits/stdc++.h>
+using namespace std;
+
+string longest_palindrome(const string &s){
+    int n = (int)s.size();
+    vector<array<int, 2>> dp(n);
+
+    pair<int, int> odd(0, -1), even(0, -1);
+    pair<int, int> ans;
+
+    for (int i = 0; i < n; i++){
+        int k = 0;
+        if(i > odd.second) k = 1;
+        else k = min(dp[odd.first+odd.second - i][0], odd.second-i+1);
+        
+
+        while(i-k >= 0 && i+k < n && s[i-k] == s[i+k]) k++;
+
+        dp[i][0] = k--;
+
+        if(i+k > odd.second) odd = {i-k, i+k};
+
+        if(2*dp[i][0]-1 > ans.second)ans = {i-k, 2*dp[i][0]-1};
+        k = 0;
+
+        if(i <= even.second) k = min(dp[even.first+even.second-i+1][1], even.second-i+1);
+
+        while(i-k-1 >= 0 && i+k < n && s[i-k-1] == s[i+k]) k++;
+
+        dp[i][1] = k--;
+
+        if(i + k > even.second) even = {i - k - 1, i + k};
+        
+        if(2 * dp[i][1] > ans.second) ans = {i-k-1, 2*dp[i][1]};
+    }
+    return s.substr(ans.first, ans.second);
+}
+
+
+
+
+
+int main(){
+    int n; cin >> n;
+    string s; cin >> s;
+
+    string res = longest_palindrome(s);
+
+    cout << res.length() << endl;
+}
