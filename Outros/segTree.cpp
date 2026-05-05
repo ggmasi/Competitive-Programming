a31e926061aedeadf7d0b977c5bfab7c0f088e37
ggmasi <ggmasi123@gmail.com>
2026-05-05 19:56:57 -0300

    Adição do algoritmo basico de segtree

---
+++ Outros/segTree.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 2e5 + 5;
+int arr[MAXN];
+int tree[4*MAXN];
+
+void build(int no, int start, int end){
+    if(start == end){
+        tree[no] = arr[start];
+    }else{
+        int mid = (start+end)/2;
+        build(no*2, start, mid);
+        build(no*2+1, mid+1, end);
+        tree[no] = tree[no*2]+tree[no*2+1];
+    }
+}
+
+void update(int no, int start, int end, int idx, int val){
+    if(start == end){
+        tree[no] = val;
+    }else{
+        int mid = (start+end)/2;
+        if(idx <= mid){
+            update(no*2, start, mid, idx, val); 
+        }else{
+            update(no*2+1, mid+1, end, idx, val);
+        }
+
+        tree[no] = tree[no*2]+tree[no*2+1];
+    }
+}
+
+int query(int no, int start, int end, int l, int r){
+    if(l > r){
+        return 0;
+    }if(l == start && r == end){
+        return tree[no];
+    }
+    int mid = (start+end)/2;
+    return query(no*2, start, mid, l, min(r, mid)) + query(no*2+1, mid+1, end, max(l, mid+1), r);
+}
+
+int main(){
+    int n = 6;
+    arr[0] = 1;
+    arr[1] = 3;
+    arr[2] = 5;
+    arr[3] = 7;
+    arr[4] = 9;
+    arr[5] = 11;
+
+    build(1, 0, n-1);
+
+    cout << "Soma de arr[1...3]: " << query(1, 0, n - 1, 1, 3) << "\n";
+
+    cout << "Soma de arr[0...5]: " << query(1, 0, n - 1, 0, n - 1) << "\n";
+
+    update(1, 0, n - 1, 2, 10);
+
+    cout << "Soma de arr[1...3]: " << query(1, 0, n - 1, 1, 3) << "\n";
+
+    cout << "Soma de arr[0...5]: " << query(1, 0, n - 1, 0, n - 1) << "\n";
+
+    return 0;
+}
