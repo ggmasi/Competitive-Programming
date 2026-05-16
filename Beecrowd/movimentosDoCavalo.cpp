ccc2fe37b64770b1f21cbd48da32bb5e296c70ff
ggmasi <ggmasi123@gmail.com>
2026-05-15 22:41:28 -0300

    movimentosDoCavalo

--- Beecrowd/movimentosDoCavalo.cpp
+++ Beecrowd/movimentosDoCavalo.cpp
@@ -6,35 +6,48 @@ int main(){
     char xi, yi, xf, yf;
     char espaco;
     while(cin >> xi){
-        cin >> yi >> espaco >> xf >> yf;
+        cin >> yi;
+        cin.ignore();
+        cin >> xf >> yf;
         
         queue<int> fila;
-        int dest = ((xf-96)*10)+(yf-48);
-        fila.push(((xi-96)*10)+(yi-48));
-        int cnt = 0;
-        while(!fila.empty()){
-            cnt++;
+        int dest = ((xf-97))+(yf-49)*10;
+        fila.push(((xi-97))+(yi-49)*10);
+
+        int cnt = 1;
+        int tam = 1;
+        if(dest == fila.front()){
+            cout << "To get from " << xi << yi << " to " << xf << yf << " takes 0 knight moves." << endl;
+            continue;
+        }
+        while(!fila.empty() ){
             int atual = fila.front();
             fila.pop();
-            if(atual > 18 && atual < 60){
-                if(atual%10 > 2 && atual%10 < 6){
-                    if(atual-21 == dest) break;
-                    fila.push(atual-21);
-                    if(atual+21 == dest) break;
-                    fila.push(atual+21);
-                    if(atual+12 == dest) break;
-                    fila.push(atual+21);
-                    if(atual-12 == dest) break;
-                    fila.push(atual-12);
-                    if(atual+8 == dest) break;
-                    fila.push(atual+8);
-                    if(atual-8 == dest) break;
-                    fila.push(atual-8);
-                }
+            tam--;
+            if(atual-21 == dest) break;
+            if((atual-21)/10 >= 0 && (atual-21)/10 <= 7 && (atual-21)%10 >= 0 && (atual-21)%10 <= 7) fila.push(atual-21);
+            if(atual+21 == dest) break;
+            if((atual+21)/10 >= 0 && (atual+21)/10 <= 7 && (atual+21)%10 >= 0 && (atual+21)%10 <= 7) fila.push(atual+21);
+            if(atual+12 == dest) break;
+            if((atual+12)/10 >= 0 && (atual+12)/10 <= 7 && (atual+12)%10 >= 0 && (atual+12)%10 <= 7) fila.push(atual+12);
+            if(atual-12 == dest) break;
+            if((atual-12)/10 >= 0 && (atual-12)/10 <= 7 && (atual-12)%10 >= 0 && (atual-12)%10 <= 7) fila.push(atual-12);
+            if(atual+8 == dest) break;
+            if((atual+8)/10 >= 0 && (atual+8)/10 <= 7 && (atual+8)%10 >= 0 && (atual+8)%10 <= 7) fila.push(atual+8);
+            if(atual-8 == dest) break;
+            if((atual-8)/10 >= 0 && (atual-8)/10 <= 7 && (atual-8)%10 >= 0 && (atual-8)%10 <= 7) fila.push(atual-8);
+            if(atual+19 == dest) break;
+            if((atual+19)/10 >= 0 && (atual+19)/10 <= 7 && (atual+19)%10 >= 0 && (atual+19)%10 <= 7) fila.push(atual+19);
+            if(atual-19 == dest) break;
+            if((atual-19)/10 >= 0 && (atual-19)/10 <= 7 && (atual-19)%10 >= 0 && (atual-19)%10 <= 7) fila.push(atual-19);
+            if(tam == 0){
+                cnt++;
+                tam = fila.size();
             }
+
         }
 
-
+        cout << "To get from " << xi << yi << " to " << xf << yf << " takes " << cnt << " knight moves." << endl;
 
         cin.ignore();
     }
