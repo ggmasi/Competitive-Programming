2e7770d684d84cdf861b75b24b4072e326a46745
ggmasi <ggmasi123@gmail.com>
2026-05-27 11:42:56 -0300

    listRemoval

--- CSES/listRemovals.cpp
+++ CSES/listRemovals.cpp
@@ -18,45 +18,21 @@ void build(int no, int start, int end){
     }
 }
 
-void update(int no, int start, int end, int idx, int val){
+int update(int no, int start, int end, int idx){
+    tree[no].first--; 
     if(start == end){
-        tree[no] = {val, arr[start]};
-    }else{
-        int mid = (start+end)/2;
-        if(idx <= mid){
-            update(no*2, start, mid, idx, val); 
-        }else{
-            update(no*2+1, mid+1, end, idx, val);
-        }
-
-        tree[no].first = tree[no*2].first+tree[no*2+1].first;
-        tree[no].second = -1;
-    }
-}
-
-int query(int no, int idx){
-    if(tree[no].second != -1){
-        
-
-        
         return tree[no].second;
-    } 
-    
+    }
+
     pair<int, int> l = tree[no*2];
-    pair<int, int> r = tree[no*2+1];
 
-    if(l.first >= idx) return query(no*2, idx);
-    else return query(no*2+1, idx-l.first);
+    int mid = (start+end)/2;
 
+    if(l.first >= idx) return update(no*2, start, mid, idx);
+    else return update(no*2+1, mid+1, end, idx-l.first);
 
-    // if(l > r){
-    //     return 0;
-    // }if(l == start && r == end){
-    //     return tree[no];
-    // }
-    // int mid = (start+end)/2;
-    // return query(no*2, start, mid, l, min(r, mid)) + query(no*2+1, mid+1, end, max(l, mid+1), r);
 }
+
 
 
 
@@ -70,8 +46,8 @@ int main(){
 
     for (int i = 0; i < n; i++){
         int temp; cin >> temp;
-        cout << query(1, temp) << endl;
-        update(1, 0, n-1, temp, 0);
+        cout << update(1, 0, n-1, temp) << " ";
+        
     }
 
     // for (int i = 0; i < n*4; i++){
