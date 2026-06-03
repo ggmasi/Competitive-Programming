bfc397a3d6e581e9b8e4fd8f2a62f56e93d606c0
ggmasi <ggmasi123@gmail.com>
2026-06-03 15:34:08 -0300

    kefa and park

--- Codeforces/kefaAndPark.cpp
+++ Codeforces/kefaAndPark.cpp
@@ -5,26 +5,28 @@ using namespace std;
 int n, m;
 map<int, vector<int>> tree;
 vector<int> cats;
+vector<int> visitados;
 
 int dfs(int root, int cntAtual){
+    visitados[root] = 1;
     if(cntAtual > m) return 0;
-    if(tree[root].size() == 0){
-        if(root == 0) return 0;
-        return 1;
-    } 
+    if(root != 0 && tree[root].size() <= 1) return 1;
 
     if(cats[root] == 0) cntAtual = 0;
 
     int sum = 0;
     for(int u : tree[root]){
-        sum += dfs(u, cntAtual+cats[u]);
+        if(visitados[u] == 0){
+            sum += dfs(u, cntAtual+cats[u]);
+        }
     }
-
     return sum;
 }
 
 int main(){
     cin >> n >> m;
+
+    visitados.assign(n, 0);
 
     for (int i = 0; i < n; i++){
         int temp; cin >> temp;
@@ -36,8 +38,10 @@ int main(){
         int u, v; cin >> u >> v;
 
         tree[u-1].push_back(v-1);
+        tree[v-1].push_back(u-1);
     }
     
+
     cout << dfs(0, cats[0]) << endl;
     
 
