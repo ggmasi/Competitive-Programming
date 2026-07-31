947c5f1ee0efb3bcafaba4d017cbacf7df1cd07f
ggmasi <ggmasi123@gmail.com>
2026-07-31 17:35:54 -0300

    ex diario + planilhaB

---
+++ Codeforces/1190A_tokitsukazeAndDiscardItems.cpp
@@ -0,0 +1,27 @@
+//https://codeforces.com/problemset/problem/1190/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int n, m, k; cin >> n >> m >> k;
+    vector<long long int> v(m);
+    for (int i = 0; i < m; i++){
+        cin >> v[i];
+    }
+
+    long long int ans = 0;
+    long long int removidos = 0;
+    long long int idx = 0;
+    while(idx < m){
+        long long int pagAtual = (v[idx]-1-removidos)/k;
+        long long int numAtual = 0;
+        while(idx < m && (v[idx]-1-removidos)/k == pagAtual){
+            numAtual++;
+            idx++;
+        }
+        removidos += numAtual;
+        ans++;
+    }
+
+    cout << ans << "\n";
+}
